77810e03cdd15c92421be20387b757ec75f2268e
kdugaspar <kadugaspardesousa@gmail.com>
2024-02-11 19:17:02 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 
    alert("Seja bem-vindo(a) ao meu site!") 
+
+   let c = window.document.querySelector('input')
+   c.addEventListener('click',soma)
+   function soma(){
+    let res = window.document.querySelector('p')
+
+    let valor = prompt("Digite um número:")
+    res.innerHTML = `O dobro de ${valor} é ${dobro(valor)} e a metade é ${metade(valor)}!`
+   }
+
+   function dobro(d){
+   return d * 2
+   }
+   function metade(m){
+    return m / 2
+   }
